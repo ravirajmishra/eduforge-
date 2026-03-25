7a20104c832c23d02d83f14809659c612757d093
Ravi raj mishra <108410923+ravirajmishra@users.noreply.github.com>
2026-03-26 01:46:20 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar() {
               Features
             </a>
             <div className="w-px h-4 bg-indigo-100" />
-            <a
+            
             <a href="#generator" className="btn-primary px-4 py-2 text-sm">
               Start Creating
             </a>
